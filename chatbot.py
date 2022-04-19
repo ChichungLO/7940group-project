6146534cc02853a697fb667b3f1dcc96e85026f8
LAO ZhiCong <winson.lo@life.hkbu.edu.hk>
2022-04-20 02:56:47 +0800

    try to fix error

--- chatbot.py
+++ chatbot.py
@@ -1,5 +1,4 @@
 #chatbot_test.py
-import telegram
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext, CallbackQueryHandler, InlineQueryHandler
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
