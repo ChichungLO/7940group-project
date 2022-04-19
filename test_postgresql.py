457a0c10cfcb3cee7938c397fa981a59ef0fe439
LAO ZhiCong <winson.lo@life.hkbu.edu.hk>
2022-04-19 19:03:34 +0800

    add postgresql

---
+++ test_postgresql.py
@@ -0,0 +1,10 @@
+import psycopg2
+
+# 连接数据库
+conn = psycopg2.connect(dbname="d9meetj91bb2ms", user="kouyhyskzsssia",
+                       password="f21744c8bfe917ea276c6b044fcf0362e50920f32143a927489d076b6d44493a", host="ec2-44-194-4-127.compute-1.amazonaws.com", port="5432")
+
+print(conn)
+
+# 关闭连接
+conn.close()
