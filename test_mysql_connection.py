91bf747a27ce67566a75349d2bf6844b127aa7ca
LAO ZhiCong <winson.lo@life.hkbu.edu.hk>
2022-04-19 17:14:11 +0800

    no message

--- test_mysql_connection.py
+++ test_mysql_connection.py
@@ -1,14 +1,9 @@
-import pyodbc
-server = 'comp7940gp13.database.windows.net'
-database = 'comp7940gp13'
-username = 'comp7940gp13'
-password = '{comp7940_gp13}'
-driver = '{ODBC Driver 17 for SQL Server}'
-
-with pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD=' + password) as conn:
-    with conn.cursor() as cursor:
-        cursor.execute("SELECT TOP 3 name, collation_name FROM sys.databases")
-        row = cursor.fetchone()
-        while row:
-            print(str(row[0]) + " " + str(row[1]))
-            row = cursor.fetchone()
+import mysql.connector
+ 
+mydb = mysql.connector.connect(
+  host="106.52.171.168",       # 数据库主机地址
+  user="comp7940gp13",    # 数据库用户名
+  passwd="comp7940gp13"   # 数据库密码
+)
+ 
+print(mydb)
