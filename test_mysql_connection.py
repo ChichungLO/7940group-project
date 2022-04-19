a03c613573caa460ed51de92fba389c326da7c5a
LAO ZhiCong <winson.lo@life.hkbu.edu.hk>
2022-04-19 17:16:36 +0800

    update

--- test_mysql_connection.py
+++ test_mysql_connection.py
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 import mysql.connector
  
 mydb = mysql.connector.connect(
@@ -8,19 +7,3 @@ mydb = mysql.connector.connect(
 )
  
 print(mydb)
-=======
-import pyodbc
-server = 'comp7940gp13.database.windows.net'
-database = 'comp7940gp13'
-username = 'comp7940gp13'
-password = '{comp7940_gp13}'   
-driver= '{ODBC Driver 17 for SQL Server}'
-
-with pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password) as conn:
-    with conn.cursor() as cursor:
-        cursor.execute("SELECT TOP 3 name, collation_name FROM sys.databases")
-        row = cursor.fetchone()
-        while row:
-            print (str(row[0]) + " " + str(row[1]))
-            row = cursor.fetchone()
->>>>>>> Stashed changes
