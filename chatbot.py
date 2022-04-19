4db4f7fc2b001c0da04306cc6960c226db708dac
LAO ZhiCong <winson.lo@life.hkbu.edu.hk>
2022-04-20 02:49:28 +0800

    update

--- chatbot.py
+++ chatbot.py
@@ -1,4 +1,5 @@
 #chatbot_test.py
+import telegram
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext, CallbackQueryHandler, InlineQueryHandler
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
